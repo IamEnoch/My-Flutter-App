7e4172c1da0ec8b958d727fcd9504e0d1c848864
Enoch Mwangi <enoch.mwangi@outlook.com>
2023-01-10 18:35:26 +0300

    registered the routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterapp/views/login_view.dart';
 import 'package:flutterapp/views/register_view.dart';
 
 import 'firebase_options.dart';
@@ -14,6 +15,10 @@ void main() {
         primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
+      routes: {
+        '/login/': (context) => const LoginView(),
+        '/register/': (context) => const RegisterView(),
+      },
     ),
   );
 }
