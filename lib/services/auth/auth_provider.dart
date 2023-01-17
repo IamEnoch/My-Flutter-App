1e30ba720e084a72f9a32372bc57ee791ecbd1b2
Enoch Mwangi <enoch.mwangi@outlook.com>
2023-01-17 19:07:49 +0300

    auth provider contract create

---
+++ lib/services/auth/auth_provider.dart
@@ -0,0 +1,16 @@
+import 'package:flutterapp/services/auth/auth_user.dart';
+
+abstract class AuthProvider {
+  AuthUser? get currentUser;
+
+  Future<AuthUser> logIn({
+    required email,
+    required password,
+  });
+  Future<AuthUser> createUser({
+    required email,
+    required password,
+  });
+  Future<void> logOut();
+  Future<void> sendEmailVerification();
+}
