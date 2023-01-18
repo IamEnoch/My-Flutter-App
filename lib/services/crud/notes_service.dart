722310ecb2d7b62e364806ad84524c7a9b5aa46d
Enoch Mwangi <enoch.mwangi@outlook.com>
2023-01-18 19:59:30 +0300

    Created databse notes class

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -24,7 +24,47 @@ class DatabaseUser {
   //specifid type
   @override
   bool operator ==(covariant DatabaseUser other) => id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
 }
 
+class DatabaseNote {
+  final int id;
+  final int userId;
+  final String text;
+  final bool isSyncedWithCloud;
+
+  DatabaseNote({
+    required this.id,
+    required this.userId,
+    required this.text,
+    required this.isSyncedWithCloud,
+  });
+
+  DatabaseNote.fromRow(Map<String, Object?> map)
+      : id = map[idColumn] as int,
+        userId = map[userIdColumn] as int,
+        text = map[textColumn] as String,
+        isSyncedWithCloud =
+            (map[isSyncedWithCloudColumn] as int) == 1 ? true : false;
+
+  @override
+  String toString() =>
+      'Note, Id = $id, userID = $userId, isSyncedWithCloud = $isSyncedWithCloud text = $text';
+
+  //Covariant tels that it is only comparity can only be done with
+  //specifid type
+  @override
+  bool operator ==(covariant DatabaseNote other) => id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
+}
+
+const dbName = 'notes.db';
 const idColumn = 'id';
 const emailColumn = 'email';
+const userIdColumn = 'user_id';
+const textColumn = 'text';
+const isSyncedWithCloudColumn = 'is_synced_with_cloud';
