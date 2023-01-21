dbf8517317a131402ba7e736724b458e4ebc40f1
Enoch Mwangi <enoch.mwangi@outlook.com>
2023-01-21 18:02:35 +0300

    Stream builder in notes view connected to stream from notes service

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -33,32 +33,53 @@ class _NotesViewState extends State<NotesView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Main UI'),
-        actions: [
-          PopupMenuButton<MenuAction>(
-            onSelected: (value) async {
-              switch (value) {
-                case MenuAction.logout:
-                  final shouldLogout = await showLogoutDialog(context);
-                  if (shouldLogout) {
-                    await FirebaseAuth.instance.signOut();
-                    Navigator.of(context).pushNamedAndRemoveUntil(
-                      loginRoute,
-                      (_) => false,
-                    );
-                  }
-              }
-            },
-            itemBuilder: ((context) {
-              return const [
-                PopupMenuItem(value: MenuAction.logout, child: Text('Logout')),
-              ];
-            }),
-          )
-        ],
-      ),
-      body: const Text('Some text'),
-    );
+        appBar: AppBar(
+          title: const Text('Main UI'),
+          actions: [
+            PopupMenuButton<MenuAction>(
+              onSelected: (value) async {
+                switch (value) {
+                  case MenuAction.logout:
+                    final shouldLogout = await showLogoutDialog(context);
+                    if (shouldLogout) {
+                      await FirebaseAuth.instance.signOut();
+                      Navigator.of(context).pushNamedAndRemoveUntil(
+                        loginRoute,
+                        (_) => false,
+                      );
+                    }
+                }
+              },
+              itemBuilder: ((context) {
+                return const [
+                  PopupMenuItem(
+                      value: MenuAction.logout, child: Text('Logout')),
+                ];
+              }),
+            )
+          ],
+        ),
+        body: FutureBuilder(
+          future: _notesService.getOrCreateUser(email: userEmail),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                //stream builder listens to changes in your stream
+                return StreamBuilder(
+                  stream: _notesService.allNotes,
+                  builder: ((context, snapshot) {
+                    switch (snapshot.connectionState) {
+                      case ConnectionState.waiting:
+                        return const Text('Waiting for all notes');
+                      default:
+                        return const CircularProgressIndicator();
+                    }
+                  }),
+                );
+              default:
+                return const CircularProgressIndicator();
+            }
+          },
+        ));
   }
 }
