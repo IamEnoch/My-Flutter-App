607efaeab0a0c05a842fd33ad46f37d173b081be
Enoch Mwangi <enoch.mwangi@outlook.com>
2023-01-23 21:25:02 +0300

    Update auth user definition

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -3,13 +3,16 @@ import 'package:flutter/foundation.dart';
 
 @immutable
 class AuthUser {
-  final String? email;
+  final String id;
+  final String email;
   final bool isEmailVerified;
 
-  const AuthUser({required this.isEmailVerified, required this.email});
+  const AuthUser(
+      {required this.id, required this.isEmailVerified, required this.email});
 
   factory AuthUser.fromFirebase(User user) => AuthUser(
+        id: user.uid,
         isEmailVerified: user.emailVerified,
-        email: user.email,
+        email: user.email!,
       );
 }
