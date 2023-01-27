e47b2077b9b32047187408296fa80562dfd7405c
Enoch Mwangi <enoch.mwangi@outlook.com>
2023-01-27 10:05:31 +0300

    Created the new events for login and authentication

--- lib/services/auth/bloc/auth_state.dart
+++ lib/services/auth/bloc/auth_state.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/foundation.dart' show immutable;
 import 'package:flutterapp/services/auth/auth_user.dart';
+import 'package:equatable/equatable.dart';
 
 @immutable
 abstract class AuthState {
   const AuthState();
 }
 
-//auth preocess loading state
-class AuthStateLoading extends AuthState {
-  const AuthStateLoading();
+class AuthStateRegistering extends AuthState {
+  final Exception? exception;
+  const AuthStateRegistering(this.exception);
+}
+
+class AuthStateUninitialized extends AuthState {
+  const AuthStateUninitialized();
 }
 
 class AuthStateLoggedIn extends AuthState {
   final AuthUser user;
-
   const AuthStateLoggedIn(this.user);
 }
 
@@ -21,13 +25,16 @@ class AuthStateNeedVerirfication extends AuthState {
   const AuthStateNeedVerirfication();
 }
 
-class AuthStateLoggedOut extends AuthState {
+//different "states" within the same state class
+//implement equality
+class AuthStateLoggedOut extends AuthState with EquatableMixin {
   final Exception? exception;
-  const AuthStateLoggedOut(this.exception);
-}
+  final bool isLoading;
+  const AuthStateLoggedOut({
+    required this.exception,
+    required this.isLoading,
+  });
 
-class AuthStateLoggedOutFailure extends AuthState {
-  final Exception exception;
-
-  const AuthStateLoggedOutFailure(this.exception);
+  @override
+  List<Object?> get props => [exception, isLoading];
 }
