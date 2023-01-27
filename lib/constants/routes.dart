dacaf22eebc50a776fcff8f51f67c91d522d6d68
Enoch Mwangi <enoch.mwangi@outlook.com>
2023-01-27 11:19:30 +0300

    removed unneeded routes

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -1,5 +1,1 @@
-const loginRoute = '/login/';
-const registerRoute = '/register/';
-const notesRoute = '/notes/';
-const verifyEmailRoute = '/verify-email/';
 const createOrUpadateNoteRoute = '/notes/new-note/';
