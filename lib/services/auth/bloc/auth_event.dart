5205308f782923372b2387a8dfa8d8ea4c30726b
Enoch Mwangi <enoch.mwangi@outlook.com>
2023-01-27 10:07:57 +0300

    Code refactoring

--- lib/services/auth/bloc/auth_event.dart
+++ lib/services/auth/bloc/auth_event.dart
@@ -1,8 +1,13 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/foundation.dart' show immutable;
 
 @immutable
 abstract class AuthEvent {
   const AuthEvent();
+}
+
+class AuthEventSendEmailVerification extends AuthEvent {
+  const AuthEventSendEmailVerification();
 }
 
 class AuthEventInitialize extends AuthEvent {
@@ -13,6 +18,19 @@ class AuthEventLogin extends AuthEvent {
   final String email;
   final String password;
   const AuthEventLogin(this.email, this.password);
+}
+
+class AuthEventRegister extends AuthEvent {
+  final String email;
+  final String password;
+  const AuthEventRegister(
+    this.email,
+    this.password,
+  );
+}
+
+class AuthEventShouldRegister extends AuthEvent {
+  const AuthEventShouldRegister();
 }
 
 class AuthEventLogout extends AuthEvent {
