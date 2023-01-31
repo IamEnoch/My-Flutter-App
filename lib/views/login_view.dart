500b9171f7ac991c43715ce7b0e87be8f3a25c02
Enoch Mwangi <enoch.mwangi@outlook.com>
2023-01-31 22:40:57 +0300

    Code refactoring

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutterapp/constants/routes.dart';
 import 'package:flutterapp/services/auth/auth_exceptions.dart';
 import 'package:flutterapp/services/auth/auth_service.dart';
 import 'package:flutterapp/services/auth/bloc/auth_bloc.dart';
@@ -48,7 +47,7 @@ class _LoginViewState extends State<LoginView> {
           } else if (state.isLoading && closeDialog == null) {
             _clodeDialogHandle = showLoadingDialog(
               context: context,
-              text: 'Loading',
+              text: 'I am Loading...',
             );
           }
 
